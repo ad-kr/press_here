402f2bf853e35911ddb3c49c81123c3dd817224c
adrian <bounce.adrian@gmail.com>
2025-11-20 16:41:31 +0100

    Add `And` combinator

--- src/trigger/combinators.rs
+++ src/trigger/combinators.rs
@@ -1,6 +1,32 @@
 use crate::{inputs::Inputs, trigger::TriggerBinding};
 use pastey::paste;
 
+/// A combinator that returns true only if both trigger bindings are pressed.
+#[derive(Clone, Copy)]
+pub struct And<T1: TriggerBinding, T2: TriggerBinding>(pub T1, pub T2);
+
+impl<T1: TriggerBinding + Clone, T2: TriggerBinding + Clone> TriggerBinding for And<T1, T2> {
+    fn pressed(&mut self, inputs: &Inputs) -> bool {
+        self.0.pressed(inputs) && self.1.pressed(inputs)
+    }
+
+    fn just_pressed(&mut self, inputs: &Inputs) -> bool {
+        let is_pressed = self.pressed(inputs);
+        let was_pressed = self.0.just_pressed(inputs) || self.1.just_pressed(inputs);
+        is_pressed && was_pressed
+    }
+
+    fn just_released(&mut self, inputs: &Inputs) -> bool {
+        let is_released = !self.pressed(inputs);
+        let was_released = self.0.just_released(inputs) || self.1.just_released(inputs);
+        is_released && was_released
+    }
+
+    fn clone_trigger(&self) -> Box<dyn TriggerBinding> {
+        Box::new(self.clone())
+    }
+}
+
 impl<T: TriggerBinding + Clone> TriggerBinding for Vec<T> {
     fn pressed(&mut self, inputs: &Inputs) -> bool {
         self.iter_mut().any(|binding| binding.pressed(inputs))
