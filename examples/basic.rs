e32fcbb6d37e1a1d79a0d55f599ba87b8f98554d
Adrian <bounce.adrian@gmail.com>
2025-11-21 00:18:02 +0100

    Adjust basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,25 +1,39 @@
 use bevy::prelude::*;
-use press_here::{AppExt, Axis, Pair, Trigger};
+use press_here::{AppExt, Axis, AxisBindingBuilder, Pair, Trigger};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_trigger::<Jump>((KeyCode::Space, KeyCode::Enter))
-        .add_axis::<Walk>(Pair(KeyCode::KeyA, KeyCode::KeyD))
-        .add_systems(Update, test)
+        // Space key or gamepad south button for jump
+        .add_trigger::<Jump>((KeyCode::Space, GamepadButton::South))
+        .add_axis::<Walk>((
+            // A/D keys for left/right movement
+            Pair(KeyCode::KeyA, KeyCode::KeyD),
+            // Left stick X-axis with deadzone
+            GamepadAxis::LeftStickX.deadzone(0.1),
+        ))
+        .add_systems(Update, update)
         .run();
 }
 
-pub struct Jump;
-pub struct Walk;
+struct Jump;
+struct Walk;
 
-fn test(jump: Res<Trigger<Jump>>, walk: Res<Axis<Walk>>) {
+fn update(jump: Res<Trigger<Jump>>, walk: Res<Axis<Walk>>) {
     if jump.just_pressed() {
-        println!("Jump!");
+        jump_character();
     }
 
     let walk_value = walk.value();
     if walk_value != 0.0 {
-        println!("Walking with intensity: {}", walk_value);
+        walk_character(walk_value);
     }
+}
+
+fn jump_character() {
+    println!("Character jumped!");
+}
+
+fn walk_character(value: f32) {
+    println!("Character walking with intensity: {}", value);
 }
