03b7330ac32a4433245a1a0ef0f0ab941fc86f50
adrian <bounce.adrian@gmail.com>
2025-11-22 14:14:47 +0100

    Make `Inputs` struct public

--- src/lib.rs
+++ src/lib.rs
@@ -109,6 +109,7 @@ pub use app::AppExt;
 pub use axis::{
     Axis, AxisBinding, bindings::*, builder::*, combinators::*, filters::*, modifiers::*,
 };
+pub use inputs::Inputs;
 pub use trigger::{Trigger, TriggerBinding, builder::*, combinators::*, modifiers::*};
 #[cfg(feature = "visualizer")]
 pub use visualizer::*;
